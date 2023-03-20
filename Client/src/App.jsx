dc2c00274ad6a81c391f816edfea6472f906cc49
malenacaroline <malenacaroline01@gmail.com>
2023-03-20 11:57:48 -0400

    init frontend modularization

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,6 @@
 /* globals React ReactDOM */
 
-const dateCheckRegex = new RegExp("^\\d\\d\\d\\d-\\d\\d-\\d\\d");
-const jsonDateChecker = (key, value) => {
-  if (dateCheckRegex.test(value)) return new Date(value);
-  return value;
-};
+import graphQLFetch from './graphQLFetch.js';
 
 const isNull = value => !value;
 
@@ -283,32 +279,6 @@ class EmployeeCreate extends React.Component {
         queryEmployee={this.props.queryEmployee}
       ></EmployeeForm>
     );
-  }
-}
-
-// Request to api
-async function graphQLFetch(query, variables) {
-  try {
-    const response = await fetch(window.ENV.CLIENT_API_ENDPOINT, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query, variables }),
-    });
-    const body = await response.text();
-    const result = JSON.parse(body, jsonDateChecker);
-
-    if (result.errors) {
-      const error = result.errors[0];
-      if (error.extensions.code === "BAD_USER_INPUT") {
-        const details = error.extensions.exception.errors.join("\n ");
-        alert(`${error.message}:\n ${details}`);
-      } else {
-        alert(`${error.extensions.code}: ${error.message}`);
-      }
-    }
-    return result.data;
-  } catch (e) {
-    return alert(`Error in sending data to server: ${e.message}`);
   }
 }
 
